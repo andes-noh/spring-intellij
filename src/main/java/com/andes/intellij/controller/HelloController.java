db15c55d43766522e867fffd01eb1bc3570a3456
andes-noh <nkj9693@gmail.com>
2022-09-19 17:59:35 +0900

    chore(): 웹 클라이언트 작성

--- src/main/java/com/andes/intellij/controller/HelloController.java
+++ src/main/java/com/andes/intellij/controller/HelloController.java
@@ -4,12 +4,14 @@ import com.andes.intellij.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/test")
 public class HelloController {
@@ -20,5 +22,12 @@ public class HelloController {
     {
         String res = testService.helloWorld();
         return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping("/0002")
+    public String getSimpleInfo(Model model)
+    {
+        model.addAttribute("message", "test hello world");
+        return "simple_info";
     }
 }
